b9bde5024ad0aca8b39b915508d57831a70871df
Laurent Montel <montel@kde.org>
2025-05-07 22:21:19 +0200

    Add messages.sh

---
+++ src/Messages.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org> 
+
+$EXTRACTRC `find . -name "*.ui" -o -name "*.rc"` >> rc.cpp
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/cmark-rc/'` -o $podir/kaichat.pot
+rm -f rc.cpp
