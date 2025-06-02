2a7741b7af69869656ec6c704638789985e32e5b
Laurent Montel <montel@kde.org>
2025-06-02 20:15:19 +0200

    Add autotest

---
+++ src/core/autotests/kaichatexportchatasjsonjobtest.cpp
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+#include "kaichatexportchatasjsonjobtest.h"
+#include "kaichatexportchatasjsonjob.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(KAIChatExportChatAsJsonJobTest)
+KAIChatExportChatAsJsonJobTest::KAIChatExportChatAsJsonJobTest(QObject *parent)
+    : QObject{parent}
+{
+}
