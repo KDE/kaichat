2c610bf1dbc2e451c0a633ba53c263946ece1289
Laurent Montel <montel@kde.org>
2025-06-03 19:31:34 +0200

    Start to implement markdown support

--- src/core/kaichatexportchatasmardownjob.cpp
+++ src/core/kaichatexportchatasmardownjob.cpp
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: LGPL-2.0-or-later
  */
 #include "kaichatexportchatasmardownjob.h"
+#include "kaichat_core_debug.h"
+#include <KLocalizedString>
+#include <QFile>
 
 KAIChatExportChatAsMardownJob::KAIChatExportChatAsMardownJob(QObject *parent)
     : KAIChatExportChatAsBaseJob{parent}
@@ -14,7 +17,31 @@ KAIChatExportChatAsMardownJob::~KAIChatExportChatAsMardownJob() = default;
 
 void KAIChatExportChatAsMardownJob::exportChat()
 {
-    // TODO
+    QString txt;
+    txt = QStringLiteral("# %1").arg(mInfo.chatTitle);
+    for (const auto &message : mInfo.listMessages) {
+        txt += QLatin1Char('\n');
+        switch (message.sender()) {
+        case TextAutoGenerateText::TextAutoGenerateMessage::Sender::Unknown:
+            qCWarning(KAICHAT_CORE_LOG) << "Sender invalid";
+            break;
+        case TextAutoGenerateText::TextAutoGenerateMessage::Sender::User:
+            txt += QStringLiteral("## %1").arg(i18n("User"));
+            break;
+        case TextAutoGenerateText::TextAutoGenerateMessage::Sender::Assistant:
+            txt += QStringLiteral("## %1").arg(i18n("Assistant"));
+            break;
+        case TextAutoGenerateText::TextAutoGenerateMessage::Sender::System:
+            txt += QStringLiteral("## %1").arg(i18n("System"));
+            break;
+        }
+        txt += message.content();
+    }
+    QFile data(mInfo.filename);
+    if (data.open(QFile::WriteOnly)) {
+        QTextStream out(&data);
+        out << txt;
+    }
     deleteLater();
 }
 
