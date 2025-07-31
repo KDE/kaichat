cba39ea7d34ba61110f06dcb3ed03e3ed6a0bee2
Laurent Montel <montel@kde.org>
2025-07-31 13:35:24 +0200

    const'ify variable

--- src/widgets/kaichatconfiguresettingsdialog.cpp
+++ src/widgets/kaichatconfiguresettingsdialog.cpp
@@ -52,7 +52,7 @@ void KAIChatConfigureSettingsDialog::readConfig()
 {
     create(); // ensure a window is created
     windowHandle()->resize(QSize(800, 600));
-    KConfigGroup group(KSharedConfig::openStateConfig(), QLatin1StringView(myConfigGroupName));
+    const KConfigGroup group(KSharedConfig::openStateConfig(), QLatin1StringView(myConfigGroupName));
     KWindowConfig::restoreWindowSize(windowHandle(), group);
     resize(windowHandle()->size()); // workaround for QTBUG-40584
 }
