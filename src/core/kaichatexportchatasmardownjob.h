970d70aa8e354eaca66d4f308464301d58b655b8
Laurent Montel <montel@kde.org>
2025-08-05 07:19:44 +0200

    Remove unused include

--- src/core/kaichatexportchatasmardownjob.h
+++ src/core/kaichatexportchatasmardownjob.h
@@ -5,7 +5,6 @@
  */
 #pragma once
 #include "libkaichatcore_export.h"
-#include <QObject>
 #include <TextAutoGenerateText/TextAutoGenerateExportChatBaseJob>
 
 class LIBKAICHATCORE_EXPORT KAIChatExportChatAsMardownJob : public TextAutoGenerateText::TextAutoGenerateExportChatBaseJob
