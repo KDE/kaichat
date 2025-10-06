a04b40dfc14df07fbed857a508dbbccf78db5877
Laurent Montel <montel@kde.org>
2025-10-06 19:38:12 +0200

    Prepare 0.6

--- src/widgets/whatsnew/whatsnewtranslations.cpp
+++ src/widgets/whatsnew/whatsnewtranslations.cpp
@@ -14,26 +14,7 @@ WhatsNewTranslations::~WhatsNewTranslations() = default;
 // Use by newFeaturesMD5
 QList<KLazyLocalizedString> WhatsNewTranslations::lastNewFeatures() const
 {
-    const QList<KLazyLocalizedString> info{
-        kli18n("Add Quick Ask Dialog."),
-        kli18n("Allow to download Ollama on Windows/MacOs."),
-        kli18n("Use prompt during chating."),
-        kli18n("Add shared text and webshorcut plugin."),
-        kli18n("Allow to configure plugins."),
-        kli18n("Add new update widget."),
-        kli18n("Add progress indicator in chats list."),
-        kli18n("Allow to change font size."),
-        kli18n("Add command line for adding new chat room, displaying list of instances."),
-        kli18n("Add spell checking support."),
-        kli18n("Add Notification popup when model download done."),
-        kli18n("Allow to save and restore text and scrollbar position."),
-        kli18n("Allow to save and restore current chat."),
-        kli18n("Add option for showing list of title chats."),
-        kli18n("Add tools support."),
-        kli18n("Allow to switch from chat identifier."),
-        kli18n("Add Current Date Time tool."),
-        kli18n("Add progress tool message info."),
-    };
+    const QList<KLazyLocalizedString> info{};
     return info;
 }
 
@@ -55,13 +36,40 @@ QList<WhatsNewInfo> WhatsNewTranslations::createWhatsNewInfo() const
     }
     {
         WhatsNewInfo info;
+        info.setNewFeatures({
+            i18n("Add Quick Ask Dialog."),
+            i18n("Allow to download Ollama on Windows/MacOs."),
+            i18n("Use prompt during chating."),
+            i18n("Add shared text and webshorcut plugin."),
+            i18n("Allow to configure plugins."),
+            i18n("Add new update widget."),
+            i18n("Add progress indicator in chats list."),
+            i18n("Allow to change font size."),
+            i18n("Add command line for adding new chat room, displaying list of instances."),
+            i18n("Add spell checking support."),
+            i18n("Add Notification popup when model download done."),
+            i18n("Allow to save and restore text and scrollbar position."),
+            i18n("Allow to save and restore current chat."),
+            i18n("Add option for showing list of title chats."),
+            i18n("Add tools support."),
+            i18n("Allow to switch from chat identifier."),
+            i18n("Add Current Date Time tool."),
+            i18n("Add progress tool message info."),
+        });
+        info.setVersion(u"0.5.0"_s);
+        info.setBugFixings({i18n("Fix icon on windows.")});
+
+        listInfo.append(std::move(info));
+    }
+    {
+        WhatsNewInfo info;
         QStringList lst;
         for (const KLazyLocalizedString &l : lastNewFeatures()) {
             lst += l.toString();
         }
         info.setNewFeatures(lst);
-        info.setVersion(u"0.5.0"_s);
-        info.setBugFixings({i18n("Fix icon on windows.")});
+        info.setVersion(u"0.6.0"_s);
+        info.setBugFixings({});
 
         listInfo.append(std::move(info));
     }
