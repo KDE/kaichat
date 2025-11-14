a5940864d7319a3f6ab369db8025af446ed4e3b1
Laurent Montel <montel@kde.org>
2025-11-14 08:06:30 +0100

    Add new feature

--- src/widgets/kaichatwhatsnewtranslations.cpp
+++ src/widgets/kaichatwhatsnewtranslations.cpp
@@ -22,6 +22,7 @@ QList<KLazyLocalizedString> KAIChatWhatsNewTranslations::lastNewFeatures() const
         kli18n("Allow to configure text to speech."),
         kli18n("Add tool plugin icon name."),
         kli18n("Add attachment support."),
+        kli18n("Add new icon."),
     };
     return info;
 }
