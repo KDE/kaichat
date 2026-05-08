7a6020c27374fa4f4270d1e096fb655f3f220dc5
Laurent Montel <montel@kde.org>
2026-05-08 13:17:33 +0200

    Prepare to add debug database

---
+++ src/widgets/databasedialog/kaichatdatabasedialog.cpp
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "kaichatdatabasedialog.h"
+
+KAIChatDatabaseDialog::KAIChatDatabaseDialog(QWidget *parent)
+    : QDialog(parent)
+{
+}
+
+KAIChatDatabaseDialog::~KAIChatDatabaseDialog() = default;
---
+++ src/widgets/databasedialog/kaichatdatabasedialog.h
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#pragma once
+
+#include <QDialog>
+
+class KAIChatDatabaseDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit KAIChatDatabaseDialog(QWidget *parent = nullptr);
+    ~KAIChatDatabaseDialog() override;
+};
