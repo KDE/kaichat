47c0e5fef9cc865dd638aa8ebeee413b875c667c
Laurent Montel <montel@kde.org>
2026-05-08 21:53:57 +0200

    Add widget

--- src/widgets/databasedialog/kaichatdatabasedialog.cpp
+++ src/widgets/databasedialog/kaichatdatabasedialog.cpp
@@ -5,10 +5,19 @@
  */
 
 #include "kaichatdatabasedialog.h"
-
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+using namespace Qt::Literals::StringLiterals;
 KAIChatDatabaseDialog::KAIChatDatabaseDialog(QWidget *parent)
     : QDialog(parent)
 {
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(u"mainLayout"_s);
+
+    auto button = new QDialogButtonBox(QDialogButtonBox::Close, this);
+    button->setObjectName(u"button"_s);
+    mainLayout->addWidget(button);
+    connect(button, &QDialogButtonBox::rejected, this, &KAIChatDatabaseDialog::reject);
 }
 
 KAIChatDatabaseDialog::~KAIChatDatabaseDialog() = default;
---
+++ src/widgets/databasedialog/kaichatdatabasewidget.cpp
@@ -0,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "kaichatdatabasewidget.h"
+#include <KLocalizedString>
+#include <QVBoxLayout>
+using namespace Qt::Literals::StringLiterals;
+KAIChatDatabaseWidget::KAIChatDatabaseWidget(QWidget *parent)
+    : QWidget{parent}
+{
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName("mainLayout"_L1);
+    mainLayout->setContentsMargins({});
+    mainLayout->setSpacing(0);
+}
+
+KAIChatDatabaseWidget::~KAIChatDatabaseWidget() = default;
