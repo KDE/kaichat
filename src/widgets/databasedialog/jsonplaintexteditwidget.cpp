52346301ebfdaf50792747e0340b9ec2c6039f65
Laurent Montel <montel@kde.org>
2026-05-09 10:38:05 +0200

    Add jsonplaintexteditwidget

---
+++ src/widgets/databasedialog/jsonplaintexteditwidget.cpp
@@ -0,0 +1,55 @@
+/*
+   SPDX-FileCopyrightText: 2025-2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "jsonplaintexteditwidget.h"
+#include "kaichat_widget_debug.h"
+#include <KSyntaxHighlighting/Definition>
+#include <KSyntaxHighlighting/SyntaxHighlighter>
+#include <KSyntaxHighlighting/Theme>
+#include <QVBoxLayout>
+#include <TextCustomEditor/PlainTextEditorWidget>
+using namespace Qt::Literals::StringLiterals;
+
+JsonPlainTextEditor::JsonPlainTextEditor(QWidget *parent)
+    : TextCustomEditor::PlainTextEditor(parent)
+{
+    setReadOnly(true);
+    setSearchSupport(true);
+}
+
+JsonPlainTextEditor::~JsonPlainTextEditor() = default;
+
+JsonPlainTextEditWidget::JsonPlainTextEditWidget(QWidget *parent)
+    : QWidget{parent}
+    , mPlainTextEdit(new TextCustomEditor::PlainTextEditorWidget(new JsonPlainTextEditor(parent), this))
+{
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(u"mainLayout"_s);
+    mainLayout->setContentsMargins({});
+
+    mPlainTextEdit->setObjectName(u"mPlainTextEdit"_s);
+    mPlainTextEdit->setReadOnly(true);
+    mainLayout->addWidget(mPlainTextEdit);
+
+    const KSyntaxHighlighting::Definition def = mRepo.definitionForName(u"Json"_s);
+    if (!def.isValid()) {
+        qCWarning(KAICHAT_WIDGET_LOG) << "Invalid definition name";
+    }
+
+    auto hl = new KSyntaxHighlighting::SyntaxHighlighter(mPlainTextEdit->editor()->document());
+    hl->setTheme((palette().color(QPalette::Base).lightness() < 128) ? mRepo.defaultTheme(KSyntaxHighlighting::Repository::DarkTheme)
+                                                                     : mRepo.defaultTheme(KSyntaxHighlighting::Repository::LightTheme));
+    hl->setDefinition(def);
+}
+
+JsonPlainTextEditWidget::~JsonPlainTextEditWidget() = default;
+
+void JsonPlainTextEditWidget::setPlainText(const QString &str)
+{
+    mPlainTextEdit->setPlainText(str);
+}
+
+#include "moc_jsonplaintexteditwidget.cpp"
---
+++ src/widgets/databasedialog/jsonplaintexteditwidget.h
@@ -0,0 +1,35 @@
+/*
+   SPDX-FileCopyrightText: 2025-2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+
+#include "libkaichatwidgets_private_export.h"
+#include <KSyntaxHighlighting/Repository>
+#include <TextCustomEditor/PlainTextEditor>
+namespace TextCustomEditor
+{
+class PlainTextEditorWidget;
+}
+class LIBKAICHATWIDGETS_TESTS_EXPORT JsonPlainTextEditor : public TextCustomEditor::PlainTextEditor
+{
+    Q_OBJECT
+public:
+    explicit JsonPlainTextEditor(QWidget *parent = nullptr);
+    ~JsonPlainTextEditor() override;
+};
+
+class LIBKAICHATWIDGETS_TESTS_EXPORT JsonPlainTextEditWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit JsonPlainTextEditWidget(QWidget *parent = nullptr);
+    ~JsonPlainTextEditWidget() override;
+
+    void setPlainText(const QString &str);
+
+private:
+    TextCustomEditor::PlainTextEditorWidget *const mPlainTextEdit;
+    KSyntaxHighlighting::Repository mRepo;
+};
