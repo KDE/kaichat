92c7bab1a8197da98470e4f831a4bd7ee0317292
Laurent Montel <montel@kde.org>
2026-05-10 10:03:10 +0200

    Prepare kaichatdatabasechatpendingtypedinfowidget

--- src/widgets/databasedialog/kaichatdatabasechatpendingtypedinfowidget.cpp
+++ src/widgets/databasedialog/kaichatdatabasechatpendingtypedinfowidget.cpp
@@ -5,21 +5,27 @@
  */
 
 #include "kaichatdatabasechatpendingtypedinfowidget.h"
+#include "kaichat_widget_debug.h"
 #include <KLocalizedString>
 #include <QVBoxLayout>
 #include <TextAutoGenerateText/TextAutoGenerateLocalDatabaseManager>
 #include <TextAutoGenerateText/TextAutoGenerateManager>
+#include <textautogeneratetext/textautogeneratelocalchatsdatabase.h>
 using namespace Qt::Literals::StringLiterals;
 KAIChatDatabaseChatPendingTypedInfoWidget::KAIChatDatabaseChatPendingTypedInfoWidget(TextAutoGenerateText::TextAutoGenerateManager *manager, QWidget *parent)
     : ExploreDatabaseBaseStorageWidget{parent}
     , mManager(manager)
 {
-    /*
-    mModel = mManager->databaseManager()->createRoomsModel(accountName);
-    if (mModel) {
-        setModel(mModel.get());
+    if (mManager) {
+#if 0
+        mModel = mManager->databaseManager()->chatsDatabase()->;
+        if (mModel) {
+            setModel(mModel.get());
+        } else {
+            qCWarning(KAICHAT_WIDGET_LOG) << "Model is not defined";
+        }
+#endif
     }
-    */
 }
 
 KAIChatDatabaseChatPendingTypedInfoWidget::~KAIChatDatabaseChatPendingTypedInfoWidget() = default;
