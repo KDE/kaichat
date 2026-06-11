0282454bbbde79afd6c847d519598ca36c6cd24d
Laurent Montel <montel@kde.org>
2026-06-11 07:16:10 +0200

    Add placeHolder

--- src/widgets/databasedialog/kaichatdatabaseselectchatlineedit.cpp
+++ src/widgets/databasedialog/kaichatdatabaseselectchatlineedit.cpp
@@ -4,11 +4,13 @@
  * SPDX-License-Identifier: LGPL-2.0-or-later
  */
 #include "kaichatdatabaseselectchatlineedit.h"
+#include <KLocalizedString>
 
 KAIChatDatabaseSelectChatLineEdit::KAIChatDatabaseSelectChatLineEdit(QWidget *parent)
     : QLineEdit(parent)
 {
     setClearButtonEnabled(true);
+    setPlaceholderText(i18nc("@info:placeholder", "Select Chat Name"));
 }
 
 KAIChatDatabaseSelectChatLineEdit::~KAIChatDatabaseSelectChatLineEdit() = default;
