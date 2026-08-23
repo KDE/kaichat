1fc72005954a72683ccc6bac181f4e295c0d03db
Laurent Montel <montel@kde.org>
2026-08-23 21:10:34 +0200

    Remove unused include

--- src/plugins/tools/currentdatetime/currentdatetimetoolplugin.cpp
+++ src/plugins/tools/currentdatetime/currentdatetimetoolplugin.cpp
@@ -10,7 +10,6 @@
 #include "currentdatetimetoolpluginutils.h"
 #include <KLocalizedString>
 #include <KPluginFactory>
-#include <QPointer>
 K_PLUGIN_CLASS_WITH_JSON(CurrentDateTimeToolPlugin, "textautogeneratetext_currentdatetimeplugin.json")
 
 using namespace Qt::Literals::StringLiterals;
