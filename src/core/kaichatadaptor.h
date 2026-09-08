5236b261f883c4aa89aa071b7ae2a11367f7267b
Laurent Montel <montel@kde.org>
2026-09-08 19:19:55 +0200

    Fix argument name

--- src/core/kaichatadaptor.h
+++ src/core/kaichatadaptor.h
@@ -23,7 +23,7 @@ public Q_SLOTS:
     QStringList chatsList();
     void switchToChat(const QString &chatName);
     void switchToChatId(const QByteArray &chatId);
-    void ask(const QString &chatId);
+    void ask(const QString &message);
 
 private:
     TextAutoGenerateText::TextAutoGenerateManager *const mManager;
